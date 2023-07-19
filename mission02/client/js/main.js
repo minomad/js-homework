ced8f79bbeecbc9655029d9cdd32ef98b9e84d86
minomad <93kymins@gmail.com>
2023-07-19 23:40:51 +0900

    setImage 함수 추가

--- mission02/client/js/main.js
+++ mission02/client/js/main.js
@@ -28,45 +28,55 @@ li is-active src alt 변경
 const navigation = getNode('.nav');
 const list = getNodes('.nav li');
 const nickName = getNode('.nickName');
-
+const visualImg = getNode('.visual img')
 
 //is-active 함수
 const handleSlider = (e) => {
   e.preventDefault();
+
   const target = e.target.closest('li');
   const index = attr(target, 'data-index');
-
   if (!target) return;
+
 
   list.forEach((li) => {
     removeClass(li, 'is-active');
   });
   addClass(target, 'is-active');
 
-  const colorA = data[index - 1].color[0];
-  const colorB = data[index - 1].color[1];
 
-  setBgColor(colorA, colorB);
+  setBgColor(index);
   setNameText(index);
+  setImage(index);
 };
 
 // setBgColor 함수
-function setBgColor(colorA, colorB) {
+const setBgColor = (index) => {
+  const colorA = data[index - 1].color[0];
+  const colorB = data[index - 1].color[1];
   document.body.style.background = `linear-gradient(to bottom, ${colorA}, ${colorB})`;
 }
 
 // setNameText 함수
-function setNameText(index) {
+const setNameText = (index) =>{
   const name = data[index - 1].name;
   nickName.textContent = name;
 }
 
 
-//setImage 함수
-function setImage() {
-  
-}
+// setImage 함수
+const setImage = (index) => {
+  const alt = data[index - 1].alt;
+  const imgName = data[index - 1].name;
+  const src = imgName.toLowerCase();
+
+  visualImg.src = `./assets/${src}.jpeg`;
+  visualImg.alt = alt;
+
+};
+
+// handleSlider 함수 내의 setImage 함수 호출 부분을 아래와 같이 수정합니다.
+
 
 //이벤트
 navigation.addEventListener('click', handleSlider);
-
