617754cb0460b48df4fc8dda79d2310ec570b662
minomad <93kymins@gmail.com>
2023-07-19 18:13:41 +0900

    teacher.js - 추가

--- mission01/js/teacher.js
+++ mission01/js/teacher.js
@@ -4,7 +4,6 @@ const user = {
 };
 
 /*
-
 1. email 정규표현식을 사용한 조건처리 
    - false면 해당 input에 is--invalid 추가
    - true면 해당 input에 is--invalid 제거
@@ -15,10 +14,7 @@ const user = {
 
 3. 로그인 버튼을 클릭시 user.id의 값과 input의 값을 비교
 4. 로그인 버튼을 클릭시 user.pw의 값과 input의 값을 비교
-
-
 5. 두 값이 일치 한다면 다음 페이지(welcome.html)로 이동
-
 */
 
 // @ 기호 포함, .포함 이후 2글자 이상
